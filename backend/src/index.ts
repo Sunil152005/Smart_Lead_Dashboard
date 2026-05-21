26cc102e79192fbc5f9b7a92d347e8f34ec32d38
Sunil152005 <suniljadhav152005@gmail.com>
2026-05-21 14:24:29 +0530

    Fourth Commit

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -9,13 +9,7 @@ dotenv.config();
 
 const app = express();
 
-app.use(
-  cors({
-    origin:
-      "https://smart-lead-dashboard-mocha.vercel.app",
-    credentials: true,
-  })
-);
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
