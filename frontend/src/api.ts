5127ecf2a2ff1f6dd8569049abae0f27adb34bbf
Sunil152005 <suniljadhav152005@gmail.com>
2026-05-21 11:57:49 +0530

    Second Commit

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://lead-dashboard-backend-edjs.onrender.com",
 });
 
 export default API;
